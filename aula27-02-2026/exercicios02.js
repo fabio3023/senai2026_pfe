71444492e361e595b053427a26bb40a7149e0ccc
Shaolin mestre baiano <fabio.fujimoto@aluno.senai.br>
2026-02-27 12:32:08 -0300

    Update exercicios02.js

--- aula27-02-2026/exercicios02.js
+++ aula27-02-2026/exercicios02.js
@@ -13,56 +13,40 @@ class Tarefa {
         return 'Tarefa reaberta';
     }
 }
-
 class ListaTarefas {
-    tarefa1 = null;
-    tarefa2 = null;
-    tarefa3 = null;
+    tarefas = [];
 
     adicionar(tarefa) {
-        if (this.tarefa1 == null) {
-            this.tarefa1 = tarefa;
-        } else if (this.tarefa2 == null) {
-            this.tarefa2 = tarefa;
-        } else if (this.tarefa3 == null) {
-            this.tarefa3 = tarefa;
-        } else {
-            return 'Lista cheia';
-        }
-
+        this.tarefas.push(tarefa);
         return 'Tarefa adicionada';
     }
 
     removerPorTitulo(titulo) {
-        if (this.tarefa1 && this.tarefa1.titulo == titulo) {
-            this.tarefa1 = null;
-        }
-        if (this.tarefa2 && this.tarefa2.titulo == titulo) {
-            this.tarefa2 = null;
-        }
-        if (this.tarefa3 && this.tarefa3.titulo == titulo) {
-            this.tarefa3 = null;
-        }
-
+        this.tarefas = this.tarefas.filter(t => t.titulo !== titulo);
         return 'Tarefa removida';
     }
 
-    listar() {
-        console.log(this.tarefa1);
-        console.log(this.tarefa2);
-        console.log(this.tarefa3);
+    listar(concluidas = null) {
+        if (concluidas === null) {
+            return this.tarefas;
+        }
+
+        return this.tarefas.filter(t => t.concluida === concluidas);
     }
 }
 const lista = new ListaTarefas();
 
 const t1 = new Tarefa();
 t1.titulo = 'Estudar';
+t1.descricao = 'Estudar JS';
 
 const t2 = new Tarefa();
 t2.titulo = 'Academia';
+t2.descricao = 'Treino';
 
 const t3 = new Tarefa();
 t3.titulo = 'Mercado';
+t3.descricao = 'Comprar frutas';
 
 lista.adicionar(t1);
 lista.adicionar(t2);
@@ -71,4 +55,6 @@ lista.adicionar(t3);
 t1.concluir();
 lista.removerPorTitulo('Academia');
 
-lista.listar();
+console.log('Todas:', lista.listar());
+console.log('Concluídas:', lista.listar(true));
+console.log('Pendentes:', lista.listar(false));
